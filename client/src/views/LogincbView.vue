9656a3db331c7a46876e0ea09a63b5dea7d21c6c
Yordy08 <yordisdurango@gmail.com>
2023-12-12 13:23:56 -0500

    login in frontend

--- client/src/views/LogincbView.vue
+++ client/src/views/LogincbView.vue
@@ -8,14 +8,14 @@
         </div>
         <div class="card-body">
           <!-- Formulario de inicio de sesión -->
-          <form>
+          <form @submit.prevent="submitForm">
             <div class="form-group">
               <label for="usuario">Usuario:</label>
-              <input type="text" class="form-control" id="usuario" placeholder="Ingrese su usuario">
+              <input v-model="usuario" type="text" class="form-control" id="usuario" placeholder="Ingrese su usuario">
             </div>
             <div class="form-group">
-              <label for="contrasena">Contraseña:</label>
-              <input type="password" class="form-control" id="contrasena" placeholder="Ingrese su contraseña">
+              <label  for="contrasena">Contraseña:</label>
+              <input v-model="contrasena" type="password" class="form-control" id="contrasena" placeholder="Ingrese su contraseña">
             </div>
             <button type="submit" class="btn btn-primary btn-block">Iniciar Sesión</button>
           </form>
@@ -27,32 +27,30 @@
 </template>
 
 <script>
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  var usuario = document.getElementById('usuario').value;
-  var contrasena = document.getElementById('contrasena').value;
-
-  
-  fetch('http://localhost:3000/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ usuario: usuario, contrasena: contrasena })
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Verificar la respuesta del servidor
-    if (data.autenticado) {
-      alert('Inicio de sesión exitoso');
-    } else {
-      alert('Inicio de sesión fallido. Verifica tus credenciales.');
+export default {
+  data() {
+    return {
+      usuario: '',
+      contrasena: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: this.usuario, password: this.contrasena })
+      })
+      .then(response => response.json())
+      .then(data => {
+       console.log(data)
+      })
+      .catch(error => {
+        console.error('Error en la solicitud:', error);
+      });
     }
-  })
-  .catch(error => {
-    console.error('Error en la solicitud:', error);
-  });
-});
+  }
+};
 </script>
-
