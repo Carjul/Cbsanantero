ec9e4f0d985c5a1bacfa753b846cb7652c63f4f0
Carjul <emugameplay.tv@gmail.com>
2024-02-27 23:14:50 -0500

    ..

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
+import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue'
 import router from './router'
 
+const app = createApp(App);
 
-createApp(App).use(router).mount('#app')
+app.use(
+    createAuth0({
+      domain: "dev-35dy5wz22m0uql2f.us.auth0.com",
+      clientId: "iiFhow7XesbA4pFa9u6Rqiu8VKz1mgE1",
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  );
+app.use(router)
+app.mount('#app')
