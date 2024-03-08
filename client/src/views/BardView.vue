b89b48cb75f53b5481a0ba468e85fb2a018484e4
Carjul <emugameplay.tv@gmail.com>
2024-03-08 13:12:52 -0500

    Refactor BardView.vue and LogincbView.vue

--- client/src/views/BardView.vue
+++ client/src/views/BardView.vue
@@ -2,38 +2,32 @@
   <div class="container">
     <hr>
     <div class="row">
-      <div
-        class="col-md-4"
-        v-for="bar in bares"
-        :key="bar.id"
-      >
+      <div class="col-md-4" v-for="bar in bares" :key="bar.id">
         <div class="card mb-4">
-          <img
-            @click="enviarIdGaleria(bar._id)"
-            :src="bar.image"
-            class="card-img-top"
-            alt="Bar"
-          />
+          <img @click="enviarIdGaleria(bar._id)" :src="bar.image" class="card-img-top" alt="Bar" />
           <div class="card-body">
             <h5 class="card-title">{{ bar.name }}</h5>
             <p class="card-text">{{ bar.description }}</p>
             <p class="card-text"><strong>Dirección:</strong> {{ bar.address }}</p>
+            <div v-if="customerRol === 'Cliente'">
+              <button class="btn btn-primary" data-toggle="modal" data-target="#solicitudModal"
+                @click="enviarcid(bar.customer_id, bar._id)">Obtener Servicio</button>
+            </div>
 
-            <!-- Add the button for requesting service -->
-            <button class="btn btn-primary" data-toggle="modal" data-target="#solicitudModal" @click="enviarcid(bar.customer_id, bar._id)">Obtener Servicio</button>
           </div>
         </div>
       </div>
     </div>
 
     <!-- Modal -->
-    <div class="modal" id="solicitudModal" tabindex="-1" role="dialog" aria-labelledby="solicitudModalLabel" aria-hidden="true">
+    <div class="modal" id="solicitudModal" tabindex="-1" role="dialog" aria-labelledby="solicitudModalLabel"
+      aria-hidden="true" >
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <!-- Modal Header -->
           <div class="modal-header">
             <h4 class="modal-title" id="solicitudModalLabel">Solicitar Servicio</h4>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeModal"></button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" ></button>
           </div>
 
           <!-- Modal Body -->
@@ -41,27 +35,31 @@
             <b>
               <p>Creando servicio:</p>
             </b>
-          
+
             <!-- Formulario con campos solicitados -->
             <form>
               <div class="form-group">
                 <label for="nombre">Nombres:</label>
-                <input type="text" class="form-control" id="nombre" placeholder="Describa nombre y apellido" v-model="formData.nombre">
+                <input type="text" class="form-control" id="nombre" placeholder="Describa nombre y apellido"
+                  v-model="formData.nombre" readonly>
               </div>
 
               <div class="form-group">
                 <label for="correo">Correo:</label>
-                <input type="email" class="form-control" placeholder="Escriba su correo" id="apellidos" v-model="formData.correo">
+                <input type="email" class="form-control" placeholder="Escriba su correo" id="apellidos"
+                  v-model="formData.correo" readonly>
               </div>
 
               <div class="form-group">
                 <label for="celular">Numero de celular:</label>
-                <input type="text" class="form-control" id="celular" placeholder="eje +57 304 000 4445" v-model="formData.celular">
+                <input type="text" class="form-control" id="celular" placeholder="eje +57 304 000 4445"
+                  v-model="formData.celular" readonly>
               </div>
 
               <div class="form-group">
                 <label for="description">Descripcion:</label>
-                <textarea id="description" placeholder="Describa su solicitud" rows="3" v-model="formData.descripcion"></textarea>
+                <textarea id="description" placeholder="Describa su solicitud" rows="3"
+                  v-model="formData.description"></textarea>
               </div>
 
               <button type="button" class="btn btn-success" @click="submitForm">Solicitar</button>
@@ -80,17 +78,27 @@ export default {
   data() {
     return {
       bares: [],
+      customerRol: '',
+      tipo: 'Bar',
       formData: {
-        _id: null,
         nombre: '',
         celular: '',
         correo: '',
-        descripcion: '',
+        description: '',
+        clienteId: null,
         customerId: null,
+        negocioId: null
       },
     };
   },
   mounted() {
+    this.customerRol = localStorage.getItem('usuarioRol');
+
+    this.formData.clienteId = localStorage.getItem('customerId')
+    this.formData.nombre = localStorage.getItem('nombreUsuario');
+    this.formData.correo = localStorage.getItem('correoUsuario');
+    this.formData.celular = localStorage.getItem('celularUsuario');
+
     this.fetchBares();
   },
   methods: {
@@ -108,24 +116,24 @@ export default {
     },
     enviarcid(cid, id) {
       this.formData.customerId = cid;
-      this.formData._id = id;
-      console.log(this.formData);
+      this.formData.negocioId = id;
     },
-    closeModal() {
-      this.formData = {
-        _id: null,
-        nombre: '',
-        celular: '',
-        correo: '',
-        descripcion: '',
-        customerId: null,
-      };
-    },
-    submitForm() {
-      console.log('CustomerID seleccionado:', this.selectedCustomerId);
-      console.log('Formulario enviado con datos:', this.formData);
-      this.closeModal();
-      // Puedes agregar aquí la lógica para enviar el formulario al servidor si es necesario.
+
+
+    async submitForm() {
+      try {
+        const response = await axios.post(`http://localhost:3000/pedirServicio?tipo=${this.tipo}`, this.formData);
+        console.log(response);
+        this.formData.customerId = null;
+        this.formData.negocioId = null;
+        this.formData.description = '';
+    
+      } catch (error) {
+        console.error('Error al enviar la solicitud', error);
+      }
+
+
+
     },
   },
 };
