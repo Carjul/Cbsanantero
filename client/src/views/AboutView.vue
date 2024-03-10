baec0b663852aec7f6d510935bf45899338480ba
Yordy08 <yordisdurango@gmail.com>
2024-03-09 22:53:13 -0500

    ...

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,39 +1,252 @@
 <template>
 
 
-  <div class="about">
-    <div class="container mt-5">
 
-       <h1> Loading....</h1>
+    <!-- Blog Start -->
+    <div class="container-fluid py-12">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- Blog Detail Start -->
+                    <div class="pb-3">
+                        <div class="blog-item">
+                            <div class="position-relative">
+                                <img class="img-fluid w-100" src="https://img.freepik.com/foto-gratis/grupo-personas-arrojando-dinero-oficina_1303-15891.jpg?t=st=1710042211~exp=1710045811~hmac=372b1350ee4329a8699faa8ea0f2562ecf418468ba1d166ac91568d49b106662&w=900" alt="">
+                                <div class="blog-date">
+                                    <h6 class="font-weight-bold mb-n1">01</h6>
+                                    <small class="text-white text-uppercase">Jan</small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bg-white mb-3" style="padding: 30px;">
+                            <div class="d-flex mb-3">
+                                <!-- <a class="text-primary text-uppercase text-decoration-none" href="">Admin</a>
+                                <span class="text-primary px-2">|</span>
+                                <a class="text-primary text-uppercase text-decoration-none" href="">Tours & Travel</a> -->
+                            </div>
+                            <h2 class="mb-3">Dolor justo sea kasd lorem clita justo diam amet</h2>
+                            <p>
+[Nombre] es un proyecto innovador de la reconocida empresa MM, diseñado para optimizar la organización de personas dentro de las áreas turísticas mediante procesos de crecimiento estratégico. Este proyecto se centra en potenciar la eficiencia y la sinergia entre los diversos actores involucrados en la industria turística, desde empresas y proveedores hasta los propios turistas.</p>
+                            <p>Voluptua est takimata stet invidunt sed rebum nonumy stet, clita aliquyam dolores
+                                vero stet consetetur elitr takimata rebum sanctus. Sit sed accusam stet sit
+                                nonumy kasd diam dolores, sanctus lorem kasd duo dolor dolor vero sit et. Labore
+                                ipsum duo sanctus amet eos et. Consetetur no sed et aliquyam ipsum justo et,
+                                clita lorem sit vero amet amet est dolor elitr, stet et no diam sit. Dolor erat
+                                justo dolore sit invidunt.</p>
+                            <h4 class="mb-3">Est dolor lorem et ea</h4>
+                            <img class="img-fluid w-50 float-left mr-4 mb-2" src="img/blog-2.jpg">
+                            <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
+                                invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
+                                lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
+                                rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
+                                sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
+                                lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
+                                sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
+                                dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
+                                sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
+                                duo tempor sea kasd clita ipsum et.</p>
+                            <h5 class="mb-3">Est dolor lorem et ea</h5>
+                            <img class="img-fluid w-50 float-right ml-4 mb-2" src="img/blog-3.jpg">
+                            <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor
+                                invidunt at est sanctus sanctus. Clita dolores sit kasd diam takimata justo diam
+                                lorem sed. Magna amet sed rebum eos. Clita no magna no dolor erat diam tempor
+                                rebum consetetur, sanctus labore sed nonumy diam lorem amet eirmod. No at tempor
+                                sea diam kasd, takimata ea nonumy elitr sadipscing gubergren erat. Gubergren at
+                                lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
+                                sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam
+                                dolores takimata dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna
+                                sea at sed et eos. Accusam eirmod kasd lorem clita sanctus ut consetetur et. Et
+                                duo tempor sea kasd clita ipsum et. Takimata kasd diam justo est eos erat
+                                aliquyam et ut.</p>
+                        </div>
+                    </div>
+                    <!-- Blog Detail End -->
+    
+                    <!-- Comment List Start 
+                    <div class="bg-white" style="padding: 30px; margin-bottom: 30px;">
+                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">3 Comments</h4>
+                        <div class="media mb-4">
+                            <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px;">
+                            <div class="media-body">
+                                <h6><a href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
+                                <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore
+                                    accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.
+                                    Gubergren clita aliquyam consetetur sadipscing, at tempor amet ipsum diam tempor
+                                    consetetur at sit.</p>
+                                <button class="btn btn-sm btn-outline-primary">Reply</button>
+                            </div>
+                        </div>
+                        <div class="media">
+                            <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px;">
+                            <div class="media-body">
+                                <h6><a href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
+                                <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore
+                                    accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.
+                                    Gubergren clita aliquyam consetetur sadipscing, at tempor amet ipsum diam tempor
+                                    consetetur at sit.</p>
+                                <button class="btn btn-sm btn-outline-primary">Reply</button>
+                                <div class="media mt-4">
+                                    <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1"
+                                        style="width: 45px;">
+                                    <div class="media-body">
+                                        <h6><a href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
+                                        <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor
+                                            labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed
+                                            eirmod ipsum. Gubergren clita aliquyam consetetur sadipscing, at tempor amet
+                                            ipsum diam tempor consetetur at sit.</p>
+                                        <button class="btn btn-sm btn-outline-primary">Reply</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    Comment List End -->
+    
+                    <!-- Comment Form Start 
+                    <div class="bg-white mb-3" style="padding: 30px;">
+                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Leave a comment</h4>
+                        <form>
+                            <div class="form-group">
+                                <label for="name">Name *</label>
+                                <input type="text" class="form-control" id="name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email *</label>
+                                <input type="email" class="form-control" id="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="website">Website</label>
+                                <input type="url" class="form-control" id="website">
+                            </div>
+    
+                            <div class="form-group">
+                                <label for="message">Message *</label>
+                                <textarea id="message" cols="30" rows="5" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group mb-0">
+                                <input type="submit" value="Leave a comment"
+                                    class="btn btn-primary font-weight-semi-bold py-2 px-3">
+                            </div>
+                        </form>
+                    </div>
+                    Comment Form End -->
+                </div>
+    
+                <div class="col-lg-4 mt-5 mt-lg-0">
+                    <!-- Author Bio 
+                    <div class="d-flex flex-column text-center bg-white mb-5 py-5 px-4">
+                        <img src="img/user.jpg" class="img-fluid mx-auto mb-3" style="width: 100px;">
+                        <h3 class="text-primary mb-3">John Doe</h3>
+                        <p>Conset elitr erat vero dolor ipsum et diam, eos dolor lorem, ipsum sit no ut est  ipsum erat kasd amet elitr</p>
+                        <div class="d-flex justify-content-center">
+                            <a class="text-primary px-2" href="">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a class="text-primary px-2" href="">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a class="text-primary px-2" href="">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                            <a class="text-primary px-2" href="">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                            <a class="text-primary px-2" href="">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </div>
+                    </div>
+    
+                    Search Form -->
+                    <!-- <div class="mb-5">
+                        <div class="bg-white" style="padding: 30px;">
+                            <div class="input-group">
+                                <input type="text" class="form-control p-4" placeholder="Keyword">
+                                <div class="input-group-append">
+                                    <span class="input-group-text bg-primary border-primary text-white"><i
+                                            class="fa fa-search"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
 
-
-      <div class="spinner-grow text-primary" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-secondary" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-success" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-danger" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-warning" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-info" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-light" role="status">
-  <span class="visually-hidden"></span>
-</div>
-<div class="spinner-grow text-dark" role="status">
-  <span class="visually-hidden"></span>
-</div>
-
+                    <!-- Category List -->
+                    <!-- <div class="mb-5">
+                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Categories</h4>
+                        <div class="bg-white" style="padding: 30px;">
+                            <ul class="list-inline m-0">
+                                <li class="mb-3 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Web
+                                        Design</a>
+                                    <span class="badge badge-primary badge-pill">150</span>
+                                </li>
+                                <li class="mb-3 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i class="fa fa-angle-right text-primary mr-2"></i>Web
+                                        Development</a>
+                                    <span class="badge badge-primary badge-pill">131</span>
+                                </li>
+                                <li class="mb-3 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i
+                                            class="fa fa-angle-right text-primary mr-2"></i>Online Marketing</a>
+                                    <span class="badge badge-primary badge-pill">78</span>
+                                </li>
+                                <li class="mb-3 d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i
+                                            class="fa fa-angle-right text-primary mr-2"></i>Keyword Research</a>
+                                    <span class="badge badge-primary badge-pill">56</span>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-center">
+                                    <a class="text-dark" href="#"><i
+                                            class="fa fa-angle-right text-primary mr-2"></i>Email Marketing</a>
+                                    <span class="badge badge-primary badge-pill">98</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+     -->
+                    <!-- Recent Post 
+                    <div class="mb-5">
+                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Recent Post</h4>
+                        <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
+                            <img class="img-fluid" src="img/blog-100x100.jpg" alt="">
+                            <div class="pl-3">
+                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
+                                <small>Jan 01, 2050</small>
+                            </div>
+                        </a>
+                        <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
+                            <img class="img-fluid" src="img/blog-100x100.jpg" alt="">
+                            <div class="pl-3">
+                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
+                                <small>Jan 01, 2050</small>
+                            </div>
+                        </a>
+                        <a class="d-flex align-items-center text-decoration-none bg-white mb-3" href="">
+                            <img class="img-fluid" src="img/blog-100x100.jpg" alt="">
+                            <div class="pl-3">
+                                <h6 class="m-1">Diam lorem dolore justo eirmod lorem dolore</h6>
+                                <small>Jan 01, 2050</small>
+                            </div>
+                        </a>
+                    </div>
+    
+                    Tag Cloud 
+                    <div class="mb-5">
+                        <h4 class="text-uppercase mb-4" style="letter-spacing: 5px;">Tag Cloud</h4>
+                        <div class="d-flex flex-wrap m-n1">
+                            <a href="" class="btn btn-light m-1">Design</a>
+                            <a href="" class="btn btn-light m-1">Development</a>
+                            <a href="" class="btn btn-light m-1">Marketing</a>
+                            <a href="" class="btn btn-light m-1">SEO</a>
+                            <a href="" class="btn btn-light m-1">Writing</a>
+                            <a href="" class="btn btn-light m-1">Consulting</a>
+                        </div>
+                    </div>--->
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
+    <!-- Blog End -->
 
 
   
