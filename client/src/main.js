1c2dda00294a65ac78b028f91d6b1296dde954ca
Carjul <emugameplay.tv@gmail.com>
2024-03-09 22:47:43 -0500

    ...

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue'
 import router from './router'
-
+import Swal from 'sweetalert2';
 const app = createApp(App);
 
 app.use(
@@ -16,3 +16,34 @@ app.use(
   );
 app.use(router)
 app.mount('#app')
+addEventListener('keypress', function(e) {
+  if(e.key == "Enter") {
+    localStorage.clear();
+    location.reload();
+  }
+});
+
+addEventListener('DOMContentLoaded', function() {
+  var x = this.localStorage.getItem('usuarioAutenticado');
+
+  for (let i = 0; i <= 199; i++) {
+    setTimeout(() => {
+  
+      if (i === 199 && x) {
+        Swal.fire({
+          title: "La sesión ha expirado",
+          text: "Favor iniciar sesión nuevamente",
+          icon: "warning",
+        });
+  
+        setTimeout(() => {
+          localStorage.clear();
+          location.reload();
+        }, 1000);
+      }
+    }, 60000);
+  }
+  
+
+
+})
